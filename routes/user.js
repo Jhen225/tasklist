9c1ec9613b0ca4431e2b6d799ff806f56ee10fa1
Joshua Henry <jhen103093@gmail.com>
2018-04-27 20:41:33 -0500

    Minor changes to user router

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,20 @@ const User = require('../models/User');
 let router = express.Router();
 
 router.post('/new', (req, res) => {
+
+  
+  /**
+   * TODO: Add in proper validation 
+   */
+   if(!req.body.username || !req.body.password){
+    res.json({success:false});  
+  }
+
   let user = new User({
-    firstName: 'Josh',
-    lastName: 'Black',
-    username: 'black.josh',
-    password: 'password'
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    username: req.body.username,
+    password: req.body.password
   })
   user.save()
   .then(() => {
